5de8fbb635718297b24ce0b9576127686a7e91ad
Aris <arisilias3@gmail.com>
2025-05-21 11:14:14 +0300

    Path fix

--- ml_project_util/quantization_util.py
+++ ml_project_util/quantization_util.py
@@ -3,7 +3,7 @@ from .path import path_definition
 
 def save_range(min_in, max_in, layer_name):
     BASE_PATH, PATH_DATASET, PATH_RAWDATA, PATH_JOINEDDATA, PATH_SAVEDMODELS = path_definition()
-    filepath = f'{BASE_PATH}/Docs_Reports/activation_range.json'
+    filepath = f'{BASE_PATH}/Docs_Reports/Quant/activation_range.json'
     try:
         with open(filepath, 'r') as file:
             layer_min_max = json.load(file)
