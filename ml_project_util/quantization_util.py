f0f690d828b94596b82638ffa13fc6c3531d2c15
Aris <arisilias3@gmail.com>
2025-05-21 11:43:17 +0300

    Added model name

--- ml_project_util/quantization_util.py
+++ ml_project_util/quantization_util.py
@@ -1,9 +1,9 @@
 import json
 from .path import path_definition
 
-def save_range(min_in, max_in, layer_name):
+def save_range(min_in, max_in, model_name, layer_name):
     BASE_PATH, PATH_DATASET, PATH_RAWDATA, PATH_JOINEDDATA, PATH_SAVEDMODELS = path_definition()
-    filepath = f'{BASE_PATH}/Docs_Reports/Quant/activation_range.json'
+    filepath = f'{BASE_PATH}/Docs_Reports/Quant/activation_range_{model_name}.json'
     try:
         with open(filepath, 'r') as file:
             layer_min_max = json.load(file)
