eb4dca832689385faffbc08fbaceceae48902e2d
Aris <arisilias3@gmail.com>
2025-06-07 20:48:47 +0300

    edit quant_activations

--- ml_project_util/quantization_util.py
+++ ml_project_util/quantization_util.py
@@ -1348,12 +1348,13 @@ def quant_activations(model, model_name, num_bits=8, input_shape=(224,224,3), mo
         print(f'Quantization range not found in {filepath}, recalculating.')
         # calculate and save json with ranges
         if(mode=='hw'):
-            pass
+            complete_dict = complete_dict_search(model, model_name, force=0, debug=0, mode='sv', filepath='0')
+            range_dict = complete_dict["activation_hw_range_dict"]
             # function that calculates hw range
         else:
             sampled_files = gen_sample_paths()
-            activation_range_search(sampled_files, model, model_name)
-        # save json
+            range_dict = activation_range_search(sampled_files, model, model_name)
+        # Save json
         try:
             with open(filepath, 'w') as f:
                 range_dict = json.dump(f, indent=4)
