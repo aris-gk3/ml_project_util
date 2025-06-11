fe839427c57c4e52255a493e6b369024c32abfd4
Aris <arisilias3@gmail.com>
2025-06-11 18:18:13 +0300

    & instead of "and" in condition of quant_model()

--- ml_project_util/quantization_util.py
+++ ml_project_util/quantization_util.py
@@ -1565,7 +1565,7 @@ def quant_model(model, model_name, num_bits=8, design='hw', batch_len=157, force
     dict = path_definition()
     BASE_PATH = dict['BASE_PATH']
     save = 1
-    if(ask_message==1 & BASE_PATH[:7]!='/kaggle'):
+    if(ask_message==1 and BASE_PATH[:7]!='/kaggle'):
         while True:
             response = input("Do you want to overwrite previous data? (y/n): ").strip().lower()
             if response == 'y':
